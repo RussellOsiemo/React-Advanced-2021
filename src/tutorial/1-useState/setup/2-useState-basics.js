f89f7ff1ab68f1b910fcdcc47564eb91bf7007f0
Russel Osiemo <86182360+RussellOsiemo@users.noreply.github.com>
2021-11-05 13:38:32 +0300

    UseStateBasics

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,20 +1,24 @@
 import React, { useState } from 'react';
-//u
+//use
+//component must be uppercase UseStateBasics
 //cannot call conditionally
-//must be inthe function/component body
+//must be in the function/component body
 const UseStateBasics = () => {
-  // console.log(useState('hello world'));
-  // const value = useState(1)[0];
-  // const myhandler = useState(1)[1];
-  // console.log(value,myhandler);
-  const [text,setText] = useState('random title');
+   console.log(useState('hello world'));
+   const value = useState(1)[0];
+  const myhandler = useState(1)[1];
+   console.log(value,myhandler);
+   const [text,setText] = useState('random title');
   const handleClick = () =>{
-    if(text === 'random title'){
+    setText('hello world');
+  if(text === 'random title'){
     setText('hello world');
   }else{
-    setText('random title');
+    setText('random title')
   }
-      }
+    //  setText('random title');
+   }
+  
   return <React.Fragment>
     <h1>{text}</h1>
     <button type='button' className='btn' onClick={handleClick}>Change Title</button>
