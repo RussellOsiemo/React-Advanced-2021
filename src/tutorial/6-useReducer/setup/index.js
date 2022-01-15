49d8358836af77834feafc4f02bffc52cf5be432
Russel Osiemo <russelosiemo@gmail.com>
2022-01-15 14:10:49 -0800

    useReducer Hook

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -3,12 +3,39 @@ import Modal from './Modal';
 import { data } from '../../../data';
 // reducer function
 const reducer =(state, action)=>{
-
+if (action.type==='ADD_ITEM') {
+  const newPeople = [...state.people, action.payload ];
+  return {
+    ...state,
+    people:newPeople,
+    isModalOpen:true,
+    modalContent:'Item Added',
+  };
 }
+  if (action.type ==='NO_VALUE') {
+    return {...state,
+    isModalOpen: true,
+    modalContent:'please Enter Value',
+
+  };
+  }
+  if (action.type ==='CLOSE_MODAL') {
+    return{...state,isModalOpen:false}
+    
+  };
+  if (action.type ==='REMOVE_ITEM') {
+   const newPeople = state.people.filter((person) =>
+     person.id !== action.payload);
+     return{...state,people: newPeople}
+  }
+// you are dispatching an action not in the reducer
+throw new Error ('no matching action type');
+};
+
 const defaultState={
   people: [],
   isModalOpen :false,
-  modalContent :''
+  modalContent :'',
 }
 const Index = () => {
   const[name,setName] = useState('');
@@ -17,26 +44,38 @@ const Index = () => {
     e.preventDefault();
     //check if name is empty string
     if (name) {
-      
+      const newItem = {id: new  Date().getTime().toString(),name}
+      dispatch({type:'ADD_ITEM', payload: newItem });
+      setName(''); 
     } else{
-      
+      dispatch({type:'NO_VALUE'});
     }
-  }
+  };
+   const closeModal = ()=>{
+dispatch({type:"CLOSE_MODAL"});
+   }
 
   return <>
-     {state.isModalOpen && <Modal  modalContent={state.modalContent}/>}
+     {state.isModalOpen && <Modal closeModal={closeModal}  modalContent={state.modalContent}/>}
      <form onSubmit ={handleSubmit} className='form'>
      <div>
-     <input type='text' value={name} onChange={(e)=>setName(e.target.value)}/>
+     <input
+       type='text'
+       value={name} 
+       onChange={(e)=>setName(e.target.value)}
+       />
      </div>
-     <button type='submit' className="btn">Add</button>
+     <button type='submit' >Add</button>
      
      </form>
      {state.people.map((person) =>{
-       return  <div key={person.id} className='item'>
-         <h4>{person.name}</h4>
-
+       return ( 
+       <div key={person.id} className='item' >
+        <h4>{person.name}</h4>
+        <button onClick={()=>dispatch(
+          {type:'REMOVE_ITEM',payload:person.id})}>Remove</button>
        </div>
+       );
       })}
      </>;
 };
